d40273a2eada51cb8eba5c275c9afbec38b2e864
333Nikita333 <nikitosfox333@gmail.com>
2023-04-04 23:27:18 +0300

    added: globalStyles, http requests to the backend, basic operations for registering, logging in and interacting with the contact list

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import { App } from 'components/App';
-import './index.css';
+import GlobalStyle from 'GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <GlobalStyle />
+    </BrowserRouter>
   </React.StrictMode>
 );
